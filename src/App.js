269f19ca033c9e9d9e2b07cea894ecf9a5d1e668
sIUzy <rtustore@gmail.com>
2023-04-12 10:30:16 +0800

    app.js modified ver

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
     <div className="App">  
  
       <AuthContextProvider> 
-        <ShopContextProvider>
-            <MainRoutes/>
+           <ShopContextProvider>
+              <MainRoutes/>
             </ShopContextProvider>   
       </AuthContextProvider>
   
